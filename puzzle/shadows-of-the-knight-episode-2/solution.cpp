8fc64d989e4d3e8a08200c1b22a3f4552be64265
gustav <dmitry.a.balakin@gmail.com>
2019-07-13 21:46:54 +0300

    solution testcase #1

--- puzzle/shadows-of-the-knight-episode-2/solution.cpp
+++ puzzle/shadows-of-the-knight-episode-2/solution.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +8,100 @@ using namespace std;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+struct Axis{
+    
+    Axis(int currentPosition, int maxPosition): position_(currentPosition), maxPosition_(maxPosition), minPosition_(0){}
+    
+    void calculateDirection();
+    void calculatePosition();
+    void jump(const Axis& axis);
+    void calculateMaxMin();
+    
+private:
+    
+    int position_;
+    int direction_;
+    int minPosition_;
+    int maxPosition_;
+    char status_; //Gadget readings after the last jump
+    
+    void setMaxMinWarmer();
+    void setMaxMinColder();
+    void setMaxMinSame();
+    
+};
+
+void Axis::calculateDirection(){
+    direction_ = (position_ > (maxPosition_ - position_)) ? -1 : 1;
+}
+
+//Coordinates for the next jump
+void Axis::calculatePosition(){
+    if(direction_ == 1){
+        position_ = maxPosition_ - (position_ - minPosition_);
+    }
+    else{
+        position_ = minPosition_ + (maxPosition_ - position_);
+    }
+}
+
+void Axis::jump(const Axis& axis){
+    string bombDir;
+    static bool thisX = true;
+    if(thisX){
+        cout << position_ << " " << axis.position_ << endl;
+    }
+    else{
+        cout << axis.position_ << " " << position_ << endl;
+    }
+    thisX = !thisX;
+    cin >> bombDir; cin.ignore();
+    status_ = bombDir.at(0);
+}
+
+void Axis::calculateMaxMin(){
+    switch(status_){
+    case 'W': //Jumped closer to the bomb
+        setMaxMinWarmer();
+        break;
+    case 'C'://Jumped further from the bomb
+        setMaxMinColder();
+        break;
+    case 'S':
+        setMaxMinSame();
+        break;
+    default:
+        break; 
+    }
+}
+
+void Axis::setMaxMinWarmer(){ 
+    int center = (maxPosition_ + minPosition_)/ 2;
+    direction_ == -1 ? maxPosition_ = center - (maxPosition_ + minPosition_ +1)%2 : minPosition_ = center + (maxPosition_ + minPosition_+1)%2;
+}
+
+void Axis::setMaxMinColder(){ 
+    int center = (maxPosition_ + minPosition_)/ 2;
+    direction_ == -1 ? minPosition_ = center + (maxPosition_ + minPosition_ +1)%2 : maxPosition_ = center - (maxPosition_ + minPosition_+1)%2;
+}
+
+void Axis::setMaxMinSame(){
+    int center = (maxPosition_ + minPosition_)/ 2;
+    maxPosition_ = center;
+    minPosition_ = center;
+}
+
+
+void findBomb(vector<Axis>& coords)
+{
+    for(int i = 0; i < 2; ++i){
+        coords.at(i).calculateDirection(); 
+        coords.at(i).calculatePosition(); 
+        coords.at(i).jump(coords.at((i+1)%2)); // x&y or y&x
+        coords.at(i).calculateMaxMin();
+    }
+}
 
 int main()
 {
@@ -20,15 +113,13 @@ int main()
     int X0;
     int Y0;
     cin >> X0 >> Y0; cin.ignore();
-
+    string bombDir;
+    cin >> bombDir;
+    
+    vector<Axis> coords {{X0, W-1}, {Y0, H-1}};
+    
     // game loop
-    while (1) {
-        string bombDir; // Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
-        cin >> bombDir; cin.ignore();
-
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-        cout << "0 0" << endl;
+    while (1) {        
+        findBomb(coords);
     }
 }
